f5702dc2a96ad6b352e039c25647fe0ee2eae71e
andrew lee <bluesky0308@gmail.com>
2023-11-14 21:00:39 +0800

    新增個人頁面樣板

--- prjDB_GamingForm_Show/Program.cs
+++ prjDB_GamingForm_Show/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Shop}/{action=Index}/{id?}");
+    pattern: "{controller=Member}/{action=Index}/{id?}");
 
 app.Run();
