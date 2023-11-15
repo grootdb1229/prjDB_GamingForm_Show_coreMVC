302b89f1ea4161a3263aae553eba9aeea51395d4
子倫 黃 <alan90306@gmail.com>
2023-11-15 13:47:39 +0800

    123

--- prjDB_GamingForm_Show/Controllers/HomeController.cs
+++ prjDB_GamingForm_Show/Controllers/HomeController.cs
@@ -32,11 +32,11 @@ namespace prjDB_GamingForm_Show.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //public IActionResult Login() 
-        //{
-        //    TMember user = new DbGamingFormTestContext().Members.FirstOrDefault(u => u.Email == 
-        //}
+        [HttpPost]
+        public IActionResult Login()
+        {
+            Member user = new DbGamingFormTestContext().Members.FirstOrDefault(u => u.Email ==
+        }
 
 
         public HomeController(ILogger<HomeController> logger)
