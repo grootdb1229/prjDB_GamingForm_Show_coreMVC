7a8934226688dd226f5cdd87ae331a2c71e2e0c2
辜克銘 <tom12344123@gmail.com>
2023-11-17 17:24:50 +0800

    blog套版2

--- prjDB_GamingForm_Show/Controllers/BlogController.cs
+++ prjDB_GamingForm_Show/Controllers/BlogController.cs
@@ -7,7 +7,13 @@ namespace prjDB_GamingForm_Show.Controllers
 {
     public class BlogController : Controller
     {
-
+        private readonly IWebHostEnvironment _host;
+        private readonly DbGamingFormTestContext _db;
+        public BlogController(IWebHostEnvironment host, DbGamingFormTestContext db)
+        {
+            _host = host;
+            _db = db;
+        }
         public IActionResult Index()
         {
             return View();
@@ -15,41 +21,48 @@ namespace prjDB_GamingForm_Show.Controllers
 
         public ActionResult List(int? FId)
         {
-            DbGamingFormTestContext db = new DbGamingFormTestContext();
-            db.Actions.Load();
+            
+            _db.Actions.Load();
+            _db.SubBlogs.Load();
+            _db.Blogs.Load();
+            _db.Tags.Load();
+            _db.SubTags.Load();
+            _db.ArticleActions.Load();
+            _db.Articles.Load();
             CBlogViewModel vm = new CBlogViewModel();
             if (FId == null)
             {
                 vm = new CBlogViewModel
                 {
-                    tags = db.Tags.Select(p => p),
-                    subTags = db.SubTags.Where(s => s.TagId == 4 && s.SubTagId != 14).Select(p => p),
-                    blogs = db.Blogs.Select(p => p),
-                    subBlogs = db.SubBlogs.Select(p => p),
-                    articles = db.Articles.AsEnumerable().OrderByDescending(a => a.ModifiedDate).Select(p => p),
-                    actions=db.Actions,
-                    articleActions=db.ArticleActions
+                    tags = _db.Tags.Select(p => p),
+                    subTags = _db.SubTags.Where(s => s.TagId == 4 && s.SubTagId != 14).Select(p => p),
+                    blogs = _db.Blogs.Select(p => p),
+                    subBlogs = _db.SubBlogs.Select(p => p),
+                    articles = _db.Articles.AsEnumerable().OrderByDescending(a => a.ModifiedDate).Select(p => p),
+                    actions= _db.Actions,
+                    articleActions= _db.ArticleActions
                 };
             }
             else
             {
                 vm = new CBlogViewModel
                 {
-                    tags = db.Tags.Select(p => p),
-                    subTags = db.SubTags.Where(s => s.TagId == 4 && s.SubTagId != 14).Select(p => p),
-                    blogs = db.Blogs.Where(b => b.SubTagId == FId).Select(p => p),
-                    subBlogs = db.SubBlogs.Where(s => s.Blog.SubTagId == FId).Select(p => p),
-                    articles = db.Articles.Where(a => a.SubBlog.Blog.SubTagId == FId).OrderByDescending(a => a.ModifiedDate).Select(p => p),
-                    actions = db.Actions,
-                    articleActions = db.ArticleActions
+                    tags = _db.Tags.Select(p => p),
+                    subTags = _db.SubTags.Where(s => s.TagId == 4 && s.SubTagId != 14).Select(p => p),
+                    blogs = _db.Blogs.Where(b => b.SubTagId == FId).Select(p => p),
+                    subBlogs = _db.SubBlogs.Where(s => s.Blog.SubTagId == FId).Select(p => p),
+                    articles = _db.Articles.Where(a => a.SubBlog.Blog.SubTagId == FId).OrderByDescending(a => a.ModifiedDate).Select(p => p),
+                    actions = _db.Actions,
+                    articleActions = _db.ArticleActions
                 };
             }
+            
             
             return View(vm);
         }
         public ActionResult ArticleList(int? FId, int? SFId)
         {
-            DbGamingFormTestContext db = new DbGamingFormTestContext();
+            
             CBlogViewModel vm = new CBlogViewModel();
             if (FId == null)
                 return RedirectToAction("List");
@@ -57,18 +70,18 @@ namespace prjDB_GamingForm_Show.Controllers
             {
                 vm = new CBlogViewModel
                 {
-                    blogs = db.Blogs.Where(b => b.BlogId == FId).Select(p => p),
-                    subBlogs = db.SubBlogs.Where(s => s.BlogId == FId).Select(p => p),
-                    articles = db.Articles.Where(a => a.SubBlog.BlogId == FId).OrderByDescending(a => a.ModifiedDate).Select(p => p)
+                    blogs = _db.Blogs.Where(b => b.BlogId == FId).Select(p => p),
+                    subBlogs = _db.SubBlogs.Where(s => s.BlogId == FId).Select(p => p),
+                    articles = _db.Articles.Where(a => a.SubBlog.BlogId == FId).OrderByDescending(a => a.ModifiedDate).Select(p => p)
                 };
             }
             else
             {
                 vm = new CBlogViewModel
                 {
-                    blogs = db.Blogs.Where(b => b.BlogId == FId).Select(p => p),
-                    subBlogs = db.SubBlogs.Where(s => s.BlogId == FId).Select(p => p),
-                    articles = db.Articles.Where(a => a.SubBlogId == SFId).OrderByDescending(a => a.ModifiedDate).Select(p => p)
+                    blogs = _db.Blogs.Where(b => b.BlogId == FId).Select(p => p),
+                    subBlogs = _db.SubBlogs.Where(s => s.BlogId == FId).Select(p => p),
+                    articles = _db.Articles.Where(a => a.SubBlogId == SFId).OrderByDescending(a => a.ModifiedDate).Select(p => p)
                 };
             }
             return View(vm);
@@ -77,8 +90,8 @@ namespace prjDB_GamingForm_Show.Controllers
         public ActionResult ArticleContent(int? FId)
         {
 
-            DbGamingFormTestContext db = new DbGamingFormTestContext();
-            var artcon = db.Articles.Where(a => a.ArticleId== FId)
+            
+            var artcon = _db.Articles.Where(a => a.ArticleId== FId)
                 .Select(a => a);
 
             return View(artcon);
