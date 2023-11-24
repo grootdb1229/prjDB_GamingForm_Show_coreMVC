02b100d18e3d407a53e70b389b1944662fd0abda
老朱 <vincent41103@gmail.com>
2023-11-24 16:56:50 +0800

    圖片辨識API

---
+++ prjDB_GamingForm_Show/Controllers/ApiController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjDB_GamingForm_Show.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Vision()
+        {
+            return View();
+        }
+
+        public IActionResult VisionBinary()
+        {
+            return View();
+        }
+    }
+}
