900ccfe128fe51f550615e7afa6bd8d2ccd03628
子倫 黃 <alan90306@gmail.com>
2023-11-29 17:58:59 +0800

    論壇寄信功能完成,登入完成

--- prjDB_GamingForm_Show/Models/CDictionary.cs
+++ prjDB_GamingForm_Show/Models/CDictionary.cs
@@ -16,6 +16,7 @@ namespace prjDB_GamingForm_Show.Models
         public static readonly string SK_UserID = "SK_UserID";
         public static readonly string SK_UserName = "SK_UserName";
         public static readonly string SK_Logged_User = "SK_Logged_User";
+        public static readonly string SK_Confirmed_MemberID = "SK_Confirmed_MemberID";
         public static string SK_管理者觀看會員清單頁數使用關鍵字 = "SK_管理者觀看會員清單頁數使用關鍵字";
         public static string SK_管理者觀看商品清單頁數使用關鍵字 = "SK_管理者觀看商品清單頁數使用關鍵字";
         public static string SK_管理者登入資訊使用關鍵字 = "SK_管理者登入資訊使用關鍵字";
