d5d9f7b46fde8a614d96ab40195ad9e2d8055c1b
子倫 黃 <alan90306@gmail.com>
2023-11-30 20:01:39 +0800

    JavaScript Onsubmit 研究中

--- prjDB_GamingForm_Show/Controllers/MemberController.cs
+++ prjDB_GamingForm_Show/Controllers/MemberController.cs
@@ -6,6 +6,7 @@ using prjDB_GamingForm_Show.ViewModels;
 using MailKit;
 using MailKit.Net.Smtp;
 using MimeKit;
+using Microsoft.AspNetCore.Http.Extensions;
 
 namespace prjDB_GamingForm_Show.Controllers
 {
@@ -31,7 +32,7 @@ namespace prjDB_GamingForm_Show.Controllers
         {
             //if (id == null)
             //    return RedirectToAction("Create");
-            if (id == null) 
+            if (id == null)
             {
                 return RedirectToAction("Login", "Home");
             }
@@ -43,7 +44,7 @@ namespace prjDB_GamingForm_Show.Controllers
         }
 
 
-        public IActionResult Test(int? id) 
+        public IActionResult Test(int? id)
         {
             IEnumerable<Member> datas = null;
             datas = from m in _db.Members
@@ -67,12 +68,12 @@ namespace prjDB_GamingForm_Show.Controllers
             member.Password = memberWrap.Password;
             member.Gender = memberWrap.Gender;
             string photoName = Guid.NewGuid().ToString() + ".jpg";
-            member.FImagePath = photoName; 
+            member.FImagePath = photoName;
             memberWrap.photo.CopyTo(new FileStream(_host.WebRootPath + "/MemberPhoto/" + photoName, FileMode.Create));
             _db.Members.Add(member);
             _db.SaveChanges();
             ViewBag.ID = member.MemberId;
-            return RedirectToAction("HomePage" , "Home");
+            return RedirectToAction("HomePage", "Home");
         }
 
         public IActionResult Edit(int? id)
@@ -88,14 +89,14 @@ namespace prjDB_GamingForm_Show.Controllers
         public IActionResult Edit(Member member)
         {
             Member dbmember = _db.Members.FirstOrDefault(m => m.MemberId == member.MemberId);
-            if (member == null) 
+            if (member == null)
             {
                 return RedirectToAction("Create");
             }
             //dbmember == null;
             if (dbmember != null)
             {
-                dbmember.Name  = member.Name;
+                dbmember.Name = member.Name;
                 dbmember.Phone = member.Phone;
                 dbmember.Email = member.Email;
                 dbmember.Password = member.Password;
@@ -103,13 +104,13 @@ namespace prjDB_GamingForm_Show.Controllers
             }
             return RedirectToAction("MemberPage", "Member");
         }
-
-        public IActionResult ForgetPassword() 
+        //會員忘記密碼流程
+        public IActionResult ForgetPassword()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult ForgetPassword(CForgetPasswordViewModel CF) 
+        public IActionResult ForgetPassword(CForgetPasswordViewModel CF)
         {
             var Emails = from e in _db.Members
                          select e.Email;
@@ -127,28 +128,41 @@ namespace prjDB_GamingForm_Show.Controllers
                 return RedirectToAction("Login", "Home");
             }
         }
-        public IActionResult SendEmail() 
+        public IActionResult SendEmail()
         {
             var message = new MimeMessage();
-            message.From.Add(new MailboxAddress("grootdb1229" , "grootdb1229@gmail.com"));
+            message.From.Add(new MailboxAddress("grootdb1229", "grootdb1229@gmail.com"));
             message.To.Add(new MailboxAddress("alan90306", "alan90306@gmail.com"));
             message.Subject = "Test mail of asp.net Core";
-            message.Body = new TextPart("plain")
+            message.Body = new TextPart("html")
             {
-                Text = "Hello Mailkit"
+                Text = "<!DOCTYPE html>" +
+                        "<html>" +
+                        "<h2> 電子信件已寄送 請至電子信箱進行確認 </h2>" +
+                        "<h3> 您的驗證碼 及 會員編號 </h3>" +
+                        "<p> 您的會員編號為:" + HttpContext.Session.GetInt32(CDictionary.SK_Confirmed_MemberID)+ "</p>" +
+                        "<p> 您的驗證碼為:"   + @Guid.NewGuid() + "</p>" +
+                        "</html>"
             };
 
-            using (var client = new SmtpClient()) 
+            using (var client = new SmtpClient())
             {
                 client.Connect("smtp.gmail.com", 587, false);
                 client.Authenticate("grootdb1229@gmail.com", "fmgx uucs lgkv vqxm");
                 client.Send(message);
                 client.Disconnect(true);
             }
-                return View();
+            return RedirectToAction("ValidationPage", "Member");
         }
+        public IActionResult ValidationPage()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public IActionResult ValidationPage(CValidationViewModel CV)
+        //{
 
-
+        //}
 
 
 
