c8d3bb312810cb3e8f141ede3e64e778018da5e3
老朱 <vincent41103@gmail.com>
2023-12-05 14:24:50 +0800

    更新Entities後(新增memberCollection)

---
+++ prjDB_GamingForm_Show/Models/Entities/MemberCollection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace prjDB_GamingForm_Show.Models.Entities;
+
+public partial class MemberCollection
+{
+    public int Id { get; set; }
+
+    public int MemberId { get; set; }
+
+    public int? CollectionId { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Intro { get; set; } = null!;
+
+    public string? FImagePath { get; set; }
+
+    public string MyCollection { get; set; } = null!;
+
+    public string ModifiedDate { get; set; } = null!;
+
+    public virtual Member Member { get; set; } = null!;
+}
