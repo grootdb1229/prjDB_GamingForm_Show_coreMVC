2b6b196e37733ad705c6ace33d94d469c7b8f56f
辜克銘 <tom12344123@gmail.com>
2023-12-08 16:29:48 +0800

    12/08 BLOG ADMIN NICE

--- prjDB_GamingForm_Show/Program.cs
+++ prjDB_GamingForm_Show/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddSession();
-builder.Services.AddDbContext<DbGamingFormTestContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("LocalConnection")));
+builder.Services.AddDbContext<DbGamingFormTestContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolConnection")));
 builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddSignalR();
 var app = builder.Build();
@@ -33,6 +33,6 @@ app.MapControllerRoute(
 //pattern: "{controller=Blog}/{action=List}/{id?}");
 //pattern: "{controller=Shop}/{action=Index}/{id?}");
 //pattern: "{controller=Depute}/{action=DeputeList}/{id?}");
-pattern: "{controller=depute}/{action=homeframe}/{id?}");
+pattern: "{controller=Admin}/{action=Index}/{id?}");
 
 app.Run();
---
+++ prjDB_GamingForm_Show/ViewModels/CAdminBlogViewModel.cs
@@ -0,0 +1,21 @@
+﻿using prjDB_GamingForm_Show.Models.Entities;
+
+namespace prjDB_GamingForm_Show.ViewModels
+{
+    public class CAdminBlogViewModel
+    {
+        public IFormFile Photo { get; set; }
+
+        public int BlogId { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public int SubTagId { get; set; }
+
+        public string? FImagePath { get; set; }
+
+
+
+
+    }
+}
