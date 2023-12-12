0b3cab76e3f2b8fe45760375eae562888076c17a
andrew lee <bluesky0308@gmail.com>
2023-12-12 13:44:36 +0800

    v1

--- prjDB_GamingForm_Show/Program.cs
+++ prjDB_GamingForm_Show/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddSession();
-builder.Services.AddDbContext<DbGamingFormTestContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("LocalConnection")));
+builder.Services.AddDbContext<DbGamingFormTestContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolConnection")));
 builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddSignalR();
 var app = builder.Build();
@@ -30,9 +30,9 @@ app.MapHub<ChatHub>("/chatHub");
 app.MapControllerRoute(
     name: "default",
 //pattern: "{controller=depute}/{action=deputemain}/{id?}");
-pattern: "{controller=Blog}/{action=List}/{id?}");
+//pattern: "{controller=Blog}/{action=List}/{id?}");
 //pattern: "{controller=Shop}/{action=Index}/{id?}");
 //pattern: "{controller=Depute}/{action=DeputeList}/{id?}");
-//pattern: "{controller=Admin}/{action=Index}/{id?}");
+pattern: "{controller=Admin}/{action=Index}/{id?}");
 
 app.Run();
