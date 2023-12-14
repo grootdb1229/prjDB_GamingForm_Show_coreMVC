e87a3a7ea465636ea78c9c36736436852d0ff462
語喬 郭 <kakuc0e0ig@gmail.com>
2023-12-14 15:06:56 +0800

    1214

--- prjDB_GamingForm_Show/Controllers/AdminController.cs
+++ prjDB_GamingForm_Show/Controllers/AdminController.cs
@@ -225,7 +225,7 @@ namespace prjDB_GamingForm_Show.Controllers
         public IActionResult ProductComplain()
         { 
             List<CProductComplainViewModel> ProductComplain = new List<CProductComplainViewModel>();
-            var datas = _db.ProductComplains.OrderBy(x=>x.Id).Select(x =>new {x.Id,x.ProductId,x.MemeberId,x.ReplyContent,x.ReportDate } );
+            var datas = _db.ProductComplains.OrderBy(x=>x.Id).Select(x =>new {x.Id,x.ProductId,x.MemeberId,x.ReplyContent,x.ReportDate ,x.Status.Name} );
             CProductComplainViewModel pc = new CProductComplainViewModel();
             foreach (var data in datas)
             {
@@ -234,6 +234,7 @@ namespace prjDB_GamingForm_Show.Controllers
                pc.MemeberId= data.MemeberId;
                pc.ReplyContent= data.ReplyContent;
                pc.ReportDate= data.ReportDate;
+               pc.Status = data.Name;
                ProductComplain.Add(pc);
             }
             return View(ProductComplain);
