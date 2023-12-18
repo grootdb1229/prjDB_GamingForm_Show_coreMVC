cc43ac2df71427fcd142afdeab9a26377316d84b
彥霖 <bluesky0308@gmail.com>
2023-12-18 23:23:28 +0800

    member聊天室完工 - 準備寫系統通知

---
+++ prjDB_GamingForm_Show/ViewModels/CPublicChatUseViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace prjDB_GamingForm_Show.ViewModels
+{
+    public class CPublicChatUseViewModel
+    {
+        public string SenderName { get; set; }
+        public string Content {  get; set; }
+        public string ModefiedDate {  get; set; }
+    }
+}
