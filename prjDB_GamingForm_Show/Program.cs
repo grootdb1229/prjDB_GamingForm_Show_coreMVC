503128a188f57bc4e0cd3a6fa02263e081c8211f
OO <sponge6308@gmail.com>
2023-12-22 01:21:14 +0800

    小推一版

--- prjDB_GamingForm_Show/Program.cs
+++ prjDB_GamingForm_Show/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.//
 builder.Services.AddControllersWithViews();
 builder.Services.AddSession();
-builder.Services.AddDbContext<DbGamingFormTestContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolConnection")));
+builder.Services.AddDbContext<DbGamingFormTestContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("LocalConnection")));
 builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddSignalR();
 var app = builder.Build();
@@ -34,7 +34,7 @@ app.MapControllerRoute(
 //pattern: "{controller=Blog}/{action=List}/{id?}");
 pattern: "{controller=Shop}/{action=Index}/{id?}");
 //pattern: "{controller=Depute}/{action=DeputeMain}/{id?}");
-pattern: "{controller=Admin}/{action=Index}/{id?}");
+//pattern: "{controller=Admin}/{action=Index}/{id?}");
 //pattern: "{controller=Home}/{action=Homepage}/{id?}");
 
 app.Run();
