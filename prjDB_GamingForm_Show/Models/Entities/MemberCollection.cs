d005bacc8a9e4834c3421798b54765d178cafcb6
子倫 黃 <alan90306@gmail.com>
2023-12-26 15:24:29 +0800

    小修 修改作品列

--- prjDB_GamingForm_Show/Models/Entities/MemberCollection.cs
+++ prjDB_GamingForm_Show/Models/Entities/MemberCollection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace prjDB_GamingForm_Show.Models.Entities;
 
@@ -8,15 +9,15 @@ public partial class MemberCollection
     public int Id { get; set; }
 
     public int MemberId { get; set; }
-
+    
     public int? CollectionId { get; set; }
-
+    [DisplayName("標題")]
     public string Title { get; set; } = null!;
-
+    [DisplayName("介紹")]
     public string Intro { get; set; } = null!;
 
     public string? FImagePath { get; set; }
-
+    [DisplayName("作品介紹")]
     public string MyCollection { get; set; } = null!;
 
     public string ModifiedDate { get; set; } = null!;
